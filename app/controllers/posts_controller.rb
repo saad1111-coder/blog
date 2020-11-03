f68008224dece7632fd772902a655679538ab03c
saadsaddique <saadsaddique8@gmail.com>
2020-11-03 18:06:03 +0500

    add post and show

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,9 +2,25 @@ class PostsController < ApplicationController
     def index 
     end
     def new
+        @post = Post.new
     end
     def create
+        @post = Post.new(post_param)
+        if @post.save
+            redirect_to @post
+        else
+            render 'new'
+        end
     end
+    def show
+        @post = Post.find(params[:id])
+    end
+
     def delete
+    end
+    private
+    
+    def post_param
+        params.require(:post).permit(:title, :content)
     end
 end
