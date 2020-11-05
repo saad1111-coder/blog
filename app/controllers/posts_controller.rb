3a25e2b63b4041781a728119332c455cb6fcad1c
saadsaddique <saadsaddique8@gmail.com>
2020-11-05 19:09:12 +0500

    make layout masterpage

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
     before_action :authenticate_user! , except: :index
+    layout 'masterpage'
     def index 
         @post = Post.all.order("created_at DESC")
     end
